c71e1cdefb4effe93d52a0cf40e13cf8ba582e2e
avelana <o.g.kazantseva@gmail.com>
2025-10-31 08:07:09 +0300

    add ex-09-03-... internal declaration of a stuct

---
+++ 09-struct-class/ex-09-03-internal-declaration-of-a-struct-employee/src/statistics.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include <iostream>
+#include <cmath>
+
+std::tuple<double, double, double>  processDataSet(const std::vector<double>& data);
+
+#endif //STATISTICS_H
